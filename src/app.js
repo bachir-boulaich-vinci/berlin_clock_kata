0fb8e2482db083a0b9ae2fca50c3f2537adf3796
bachir-boulaich-vinci <bachir.boulaich@student.vinci.be>
2024-11-11 11:20:59 +0100

    refactor: turn on simple minutes lights while to for

--- src/app.js
+++ src/app.js
@@ -1,29 +1,18 @@
 export class Main {
     getSimpleMinutesLine(minutes){
-        // if(minutes === 1) return "JXXX";
-        // if(minutes === 2) return "JJXX";
-        // if(minutes === 3) return "JJJX";
-        // if(minutes === 4) return "JJJJ";
         let yellowCounter = minutes % 5;
-        let offCounter = 4 - yellowCounter;
+        return this.turnOnSimpleMinutesLights(yellowCounter);
+
+    }
+
+    turnOnSimpleMinutesLights(yellowCounter) {
         let result = "";
-        result = this.turnOnYellowLights(yellowCounter, result);
-        result = this.turnOffYellowLights(offCounter, result);
-        return result;
-    }
-
-    turnOffYellowLights(offCounter, result) {
-        while (offCounter > 0) {
-            result += "X";
-            offCounter--;
-        }
-        return result;
-    }
-
-    turnOnYellowLights(yellowCounter, result) {
-        while (yellowCounter > 0) {
-            result += "J";
-            yellowCounter--;
+        for (let i = 1; i <= 4; i++) {
+            if (yellowCounter >= i) {
+                result += "J";
+            } else {
+                result += "X";
+            }
         }
         return result;
     }
