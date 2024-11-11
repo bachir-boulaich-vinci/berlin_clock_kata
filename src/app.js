fa946d828d2612254064efb6ab8604da5c79671c
bachir-boulaich-vinci <bachir.boulaich@student.vinci.be>
2024-11-11 11:15:09 +0100

    refactor: five minutes line

--- src/app.js
+++ src/app.js
@@ -26,15 +26,24 @@ export class Main {
 
     getFiveMinutesLine(minutes){
         let yellowCounter = Math.floor(minutes / 5);
+        return this.turnOnFiveMinutesLights(yellowCounter);
+    }
+
+    turnOnFiveMinutesLights(yellowCounter) {
         let result = "";
-        for(let i = 1; i <= 11; i++){
-            if(yellowCounter >= i){
-                if(i % 3 === 0) result += "R";
-                else result += "J";
-            } else{
+        for (let i = 1; i <= 11; i++) {
+            if (yellowCounter >= i) {
+                result = this.isLightRedOrYellow(i, result);
+            } else {
                 result += "X";
             }
         }
+        return result;
+    }
+
+    isLightRedOrYellow(i, result) {
+        if (i % 3 === 0) result += "R";
+        else result += "J";
         return result;
     }
 }
