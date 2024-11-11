93034729ad88105084340daf22d32747fd595ff4
dejonge <marcus.dejonge@student.vinci.be>
2024-11-11 12:34:20 +0100

    Add: seconds line when seconds = 49

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -185,4 +185,9 @@ describe("Converting timestamp into Berlin Clock Kata", function(){
 
         expect(result).toBe("X");
     }); 
+    it("Seconds line: when seconds equal 49", function(){
+        const result = main.getSecondsLine(new Date("2024-11-01T23:00:49").getSeconds());
+
+        expect(result).toBe("X");
+    }); 
 });
